d7e279335ae1dc390593a7fc46a635628c2acfa1
MVR727 <mvr35688@gmail.com>
2025-05-22 09:54:48 -0300

    estrutura basica

---
+++ app/index.tsx
@@ -0,0 +1,55 @@
+import { Text, TouchableOpacity } from "react-native";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+
+
+export default function Index(){
+
+    return <View>
+        <Text>Joga da Velha</Text>
+        <Text>Vencedor</Text>
+
+        <View>
+            <View>
+                <TouchableOpacity>
+                    <Text></Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Text></Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Text></Text>
+                </TouchableOpacity>
+            </View>
+
+            <View>
+                <TouchableOpacity>
+                    <Text></Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Text></Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Text></Text>
+                </TouchableOpacity>
+                </View>
+
+            <View>
+                <TouchableOpacity>
+                    <Text></Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Text></Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Text></Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    </View>
+}
